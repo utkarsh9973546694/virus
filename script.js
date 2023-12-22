c900b06c25eae1e1470a6ac2e3fc7435db2385e1
Utkarsh Kumar Singh <146262046+utkarsh9973546694@users.noreply.github.com>
2023-12-22 16:38:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let z='GO'
+let u=prompt('Enter "GO" to continue');
+ while(u!=z){
+    u=prompt('Enter "GO" to continue'); 
+ }
+console.log('Welcome')
+for(let i=0;i>=0;i++){
+    console.log('Bewakoof');
+}
